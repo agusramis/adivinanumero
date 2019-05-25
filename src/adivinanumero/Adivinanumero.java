1590913fbf6079d504c524bff7fafb504b1ad05f
Agusramis <ramisagustin@gmail.com>
2019-05-25 19:01:20 -0300

    Revert "First commit"
    This reverts commit f6f783aa2f2b3410b1c2d85192f8093e60d0e3ec.

--- src/adivinanumero/Adivinanumero.java
+++ src/adivinanumero/Adivinanumero.java
@@ -16,5 +16,26 @@ public class Adivinanumero {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     
+     Scanner in = new Scanner(System.in);
+    Random rand = new Random();
+    int randNum = 0;
+    int upperLimit = 100;
+    int lowerLimit = 1;
+    String myAnswer = "";
+
+    do {
+        randNum = rand.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+        System.out.println("I think it's " + randNum);
+        myAnswer = in.nextLine();   
+
+        if(myAnswer.equals("tl")) {  //too low
+            lowerLimit = randNum + 1;
+        } 
+        else if(myAnswer.equals("th")) { // too high
+            upperLimit = randNum - 1;
+        }   
+    }while(!myAnswer.equals("y"));
+
+    in.close();
+    System.out.println("YAAAY! :D");
 }}
